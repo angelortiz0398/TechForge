141de768f1e1100d0ee733d5ded3ca1001974629
angelortiz0398 <angelortiz0398@gmail.com>
2024-04-29 16:16:50 -0600

    FEAT : Se agrega interfaz para manejar los contactos, y metodo para guardar nuevos contactos mediante el consumo de una api

---
+++ src/app/entities/Contacto.ts
@@ -0,0 +1,9 @@
+export interface Contacto {
+  id: number;
+  nombre: string;
+  email: string;
+  telefono: string;
+  nombreCompania: string;
+  servicioInteres: string;
+  comentarioAyuda: string;
+}
